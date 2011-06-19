c6dbef709ca430ed8576f561ec11264354e7c9a5
Edmore  Moyo <edmore.moyo@unboxedconsulting.com>
2011-06-19 10:15:43 +0200

    PW : Enabling popups of external links using custom data attributes and jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+$(function(){
+  $(".popup" ).click(function(event){
+ event.preventDefault();
+ var href = $(this).attr("href");
+ var width = $(this).attr("data-width");
+ var height = $(this).attr("data-height");
+ var popup = window.open (href,"popup",
+ "height=" + height +",width=" + width + "");
+ });
+});
+
