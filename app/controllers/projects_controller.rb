badfe8328d31e3a3021a2bc8e7a1c81c54b88bfc
Edmore  Moyo <edmore.moyo@unboxedconsulting.com>
2011-06-25 22:19:12 +0200

    Github API wrapper implementation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,8 @@
 class ProjectsController < ApplicationController
 
   def show
-
+    personal_website = Github.new("edmore", "edmoremoyo", "master")
+    @message = personal_website.last_commit_message
   end
 
 end
---
+++ app/models/github.rb
@@ -0,0 +1,88 @@
+class Github
+  require 'open-uri'
+
+  attr_accessor :user_id, :repo, :branch, :api
+
+  def initialize(user, repo, branch, api = "http://github.com/api/v2/json")
+    @user_id = user
+    @repo = repo
+    @branch = branch
+    @api = api
+  end
+
+  def list_commits
+    begin
+    open("#{@api}/commits/list/#{@user_id}/#{@repo}/#{@branch}").read
+    rescue OpenURI::HTTPError => @e
+      error_message
+    rescue Timeout::Error
+      error_message_timeout
+    end
+  end
+
+  def last_commit_message
+    begin
+    parsed_json["commits"][0]["message"]
+    rescue
+      error_message
+    end
+  end
+
+  def last_commit_date
+    begin
+    parsed_json["commits"][0]["committed_date"]
+    rescue
+      error_message
+    end
+  end
+
+  def last_commit_url
+    begin
+    parsed_json["commits"][0]["url"]
+    rescue
+      error_message
+    end
+  end
+
+  def last_committer
+    begin
+    parsed_json["commits"][0]["committer"]["name"]
+    rescue
+      error_message
+    end
+  end
+
+  def commits_on_file(file_name)
+    begin
+    open("#{@api}/commits/list/#{@user_id}/#{@repo}/#{@branch}/#{file_name}").read
+    rescue OpenURI::HTTPError => @e
+      error_message
+    rescue Timeout::Error
+      error_message_timeout
+    end
+  end
+
+  def commit(commit_id)
+    begin
+    open("#{@api}/commits/show/#{@user_id}/#{@repo}/#{commit_id}").read
+    rescue OpenURI::HTTPError => @e
+      error_message
+    rescue Timeout::Error
+      error_message_timeout
+    end
+  end
+
+  private
+  def parsed_json
+    ActiveSupport::JSON.decode(list_commits)
+  end
+
+  def error_message
+    "Oops!!, something went wrong : #{@e.message}."
+  end
+
+  def error_message_timeout
+    "Oops!!, some timeout issues, sorry."
+  end
+
+end
