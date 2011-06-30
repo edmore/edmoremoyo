9ceee26724bab2f605b8a35dd0e797f29bf61a00
Edmore  Moyo <edmore.moyo@unboxedconsulting.com>
2011-06-30 09:21:11 +0200

    Adding error handling to the Github spec file

--- spec/models/github_spec.rb
+++ spec/models/github_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'open-uri'
 
 describe Github do
 
@@ -11,6 +10,14 @@ describe Github do
   describe "#list_commits" do
     it "should return a list of commits" do
       @project.list_commits.should == "{\"commits\":[{\"parents\":[{\"id\":\"3ed85a545cfa9ea25595925103c1af077e25274e\"}],\"author\":{\"name\":\"Edmore  Moyo\",\"login\":\"edmore\",\"email\":\"etmoyo@test.com\"},\"url\":\"/edmore/edmoremoyo/commit/1f78ea9669f9910829046d925a9d02757ea36522\",\"id\":\"1f78ea9669f9910829046d925a9d02757ea36522\",\"committed_date\":\"2011-06-28T01:18:03-07:00\",\"authored_date\":\"2011-06-28T01:18:03-07:00\",\"message\":\"More refactoring of Github API wrapper class\",\"tree\":\"002f5de7e919865670b64a4548954990a9c38b9e\",\"committer\":{\"name\":\"Edmore Moyo\",\"login\":\"edmore\",\"email\":\"etmoyo@test.com\"}}]}"
+    end
+
+    it "should display a message when something goes wrong" do
+      project = Github.new("user", "repo", "branch", "http://github.com/api/v2/json")
+
+      project.stub(:open_api).with("#{@project.api}/commits/list/#{@project.user_id}/#{@project.repo}/#{@project.branch}").and_return("Oops!!, some http issues, sorry.")
+
+      project.list_commits.should == "Oops!!, some http issues, sorry."
     end
   end
 
@@ -44,6 +51,14 @@ describe Github do
       @project.stub(:open_api).with("#{@project.api}/commits/list/#{@project.user_id}/#{@project.repo}/#{@project.branch}/#{file_name}").and_return("commits_on_file")
       @project.commits_on_file(file_name).should == "commits_on_file"
     end
+
+    it "should display a message when something goes wrong" do
+      file_name = "test_file"
+      @project.stub(:open_api).with("#{@project.api}/commits/list/#{@project.user_id}/#{@project.repo}/#{@project.branch}/#{file_name}").and_return("Something went wrong")
+      @project.commits_on_file(file_name).should == "Something went wrong"
+    end
+
+
   end
 
   describe "#commit" do
@@ -51,6 +66,12 @@ describe Github do
       id = "abcxyz"
       @project.stub(:open_api).with("#{@project.api}/commits/show/#{@project.user_id}/#{@project.repo}/#{id}").and_return("commit")
       @project.commit(id).should == "commit"
+    end
+
+    it "should display a message when something goes wrong" do
+      id = "abcxyz"
+      @project.stub(:open_api).with("#{@project.api}/commits/show/#{@project.user_id}/#{@project.repo}/#{id}").and_return("Something went very wrong")
+      @project.commit(id).should == "Something went very wrong"
     end
   end
 
