5a0f91026851a64b21e6ccc88b8b113968245ffb
Edmore Moyo <edmoremoyo@c8-2a-14-23-3c-b8.lan.uct.ac.za>
2011-10-17 10:14:11 +0200

    Adding jFlickery project to projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,10 @@ class ProjectsController < ApplicationController
 
   def show
     personal_website = Github.new("edmore", "edmoremoyo", "master")
-    @message = personal_website.last_commit_message
+    @personal_message = personal_website.last_commit_message
+
+    flickery = Github.new("edmore", "jFlickery", "master")
+    @flickery_message = flickery.last_commit_message
   end
 
 end
