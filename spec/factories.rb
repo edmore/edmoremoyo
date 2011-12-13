bc636c4797e25dbe6ad8b255a88d60135a38ae61
Edmore Moyo <etmoyo@gmail.com>
2011-12-13 14:45:45 +0200

    Clearing up unnecessary whitespace in factories file

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,14 @@
 Factory.define :user do |f|
-
   f.sequence(:email) { |n| "foo#{n}@gmail.com" }
   f.password  'foobar'
   f.password_confirmation { |u| u.password }
-
 end
 
 Factory.define :profile do |f|
-
   f.summary  'My Summary'
   f.education  'Education'
   f.skills  'My Skills'
   f.experience 'My Experience'
-
 end
 
 Factory.define :post do |f|
