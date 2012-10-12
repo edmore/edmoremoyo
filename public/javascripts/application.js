1ffe80f1033b2ecb79199daf6773ccf77ff55e9a
Edmore Moyo <etmoyo@gmail.com>
2012-10-12 11:19:18 +0200

    Refactoring transfrom code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -59,9 +59,10 @@ $(function(){
 
     // Transform the header tags for all the blog links
     $("article.blogs > header").each(function(){
-	// get random number between -2 and 2
-	var rotation = Math.floor(Math.random() * (2 + 2 + 1)) -2,
-        i = 0;
+	var max = 2,
+        min = -2,
+	rotation = Math.floor(Math.random() * (max - min + 1)) + min,
+        i = 0,
         browsers = ["-o-", "-moz-", "-webkit-",""];
         for(i=0; i < browsers.length; i+=1){
 	    $(this).css(browsers[i] + "transform", "rotate(" + rotation +"deg)");
